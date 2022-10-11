acab6f90fddb2924901c1f59844036fead2ffc51
陈康烈 <kanglie.chen@jumho.cn>
2022-10-11 11:17:39 +0800

    feat: 添加mongodb启动脚本

---
+++ mongodb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# wiredTigerCacheSizeGB: 限制mongodb使用的最大内存, 如果mongodb单独部署一台设备可以忽略此选项
+
+name=mongodb
+
+docker rm -f $name >/dev/null 2>&1
+docker run \
+    -itd \
+    --restart=always \
+    --name $name \
+    -p 27017:27017 \
+    -v /data/mongodb:/var/lib/mongodb \
+    -e MONGO_INITDB_ROOT_USERNAME=root \
+    -e MONGO_INITDB_ROOT_PASSWORD=root_pwd \
+    mongo:latest \
+    --auth \
+    --wiredTigerCacheSizeGB 1
