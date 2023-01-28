e8b1416225cb77912875da92b73eefdb9577a5cf
陈康烈 <kanglie.chen@jumho.cn>
2023-01-28 14:37:02 +0800

    fix: 修正mongodb的data目录映射

--- mongodb.sh
+++ mongodb.sh
@@ -10,7 +10,7 @@ docker run \
     --restart=always \
     --name $name \
     -p 27017:27017 \
-    -v /data/mongodb:/var/lib/mongodb \
+    -v /data/mongodb:/data/db \
     -e MONGO_INITDB_ROOT_USERNAME=root \
     -e MONGO_INITDB_ROOT_PASSWORD=root_pwd \
     mongo:latest \
